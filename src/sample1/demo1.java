06f45c90436499f8ebc913437ea05cff61c6d384
Spgay <Spgay@SOMNATH>
2021-08-02 10:32:23 +0530

    i have successfully created project

---
+++ src/sample1/demo1.java
@@ -0,0 +1,8 @@
+package sample1;
+
+public class demo1 {
+	public static void main(String[] args) {
+		System.out.println("good morning");
+	}
+
+}
---
+++ src/sample1/demo2.java
@@ -0,0 +1,7 @@
+package sample1;
+
+public class demo2 {
+	public static void main(String[] args) {
+		System.out.println("good night");
+	}
+}
